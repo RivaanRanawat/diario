4df42b57333702707145a4e102193e910ee00e83
Rivaan Ranawat <rivaanranawat@Rivaans-MacBook-Air.local>
2021-08-31 22:56:32 +0530

    displaying books

--- src/components/Home/Book/BookCard.jsx
+++ src/components/Home/Book/BookCard.jsx
@@ -1,24 +1,30 @@
 import React from "react";
+import { useHistory } from "react-router";
 import styles from "./BookCard.module.css";
 
-function BookCard({bookName, date}) {
+function BookCard({ bookName, date, id }) {
+  const history = useHistory();
+  
+  function handleBookClick() {
+    history.push(`/text-editor/${id}`);
+  }
   return (
-      <div className={styles.book}>
-        <div className={styles.front}>
-          <div className={styles.cover}>
-            <div className={styles.textName}>
-              <h1 style={{ fontSize: 40 }}>{bookName}</h1>
-            </div>
-            <p className={styles.author}>{date}</p>
+    <div className={styles.book} onClick={handleBookClick}>
+      <div className={styles.front}>
+        <div className={styles.cover}>
+          <div className={styles.textName}>
+            <h1 style={{ fontSize: 40 }}>{bookName}</h1>
           </div>
-        </div>
-        <div className={styles.leftSide}>
-          <h2>
-            <span>{bookName}</span>
-            <span>{date}</span>
-          </h2>
+          <p className={styles.author}>{date}</p>
         </div>
       </div>
+      <div className={styles.leftSide}>
+        <h2>
+          <span>{bookName}</span>
+          <span>{date}</span>
+        </h2>
+      </div>
+    </div>
   );
 }
 
--- src/components/Home/Book/BookList.jsx
+++ src/components/Home/Book/BookList.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BookCard from "./BookCard";
 import styles from "./BookCard.module.css";
+import { db } from "../../../services/firebase";
+import { useAuth } from "../../../context/AuthContext";
 
 function BookList() {
-  return (
-    <div class={styles.container} style={{ display: "flex", flexWrap: "wrap", }}>
-      <BookCard
-        bookName={"Rivaan Notes"}
-        date={"22/07/21"}
-      />
-      <BookCard
-        bookName={"Rivaan Notes"}
-        date={"22/07/21"}
-      />
+  const [isLoading, setIsLoading] = useState(false);
+  const [info, setInfo] = useState([]);
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    async function fetchUserDiaries() {
+      try {
+        setIsLoading(true);
+        const querySnapshot = await db
+          .collection("diaries")
+          .where("createdBy", "==", currentUser.uid)
+          .get();
+        querySnapshot.forEach((snapshot) => {
+          setInfo((arr) => [...arr, snapshot.data()]);
+        });
+      } catch (err) {
+        setIsLoading(false);
+        alert(err.message); //
+      }
+    }
+    fetchUserDiaries().then(() => {
+      setIsLoading(false);
+      console.log(info); //
+    });
+  }, []);
+
+  return !isLoading && info !== [] ? (
+    <div class={styles.container} style={{ display: "flex", flexWrap: "wrap" }}>
+      {info.map((data) => (
+        <BookCard
+          bookName={data.name}
+          date={data.createdAt.toDate().toLocaleDateString()}
+          id={data.diaryId}
+        />
+      ))}
     </div>
+  ) : (
+    <div>Loading..</div>
   );
 }
 
